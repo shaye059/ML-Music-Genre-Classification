26db1daad9fbbbdac2b899af34b531cf120469a1
Nicolas Pare <npare056@uottawa.ca>
2019-11-02 16:08:59 -0400

    Begun working on other features to be added.

--- main/src/FFT_Converter.py
+++ main/src/FFT_Converter.py
@@ -2,7 +2,7 @@ import librosa
 import matplotlib as plt
 import librosa.display
 
-file_path = '/Users/nicolaspare/Desktop/CSI4106/Project/genres/hiphop/hiphop.00000.wav'
+file_path = '/Users/nicolaspare/GitRepo/ML-Music-Classifier/main/resources/genres/hiphop/hiphop.00000.wav'
 x , sr = librosa.load(file_path)
 
 plt.figure(figsize=(20, 6))
