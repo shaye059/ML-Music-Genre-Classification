78508fe17cdd7222ffd9074b9b37434283a0cc8a
shay059 <spencer.hayes.laverdiere@gmail.com>
2019-11-02 12:34:39 -0400

    Changes for path

--- main/src/FFT_Converter.py
+++ main/src/FFT_Converter.py
@@ -1,8 +1,8 @@
 import librosa
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import librosa.display
 
-file_path = '/Users/nicolaspare/Desktop/CSI4106/Project/genres/hiphop/hiphop.00000.wav'
+file_path = '../resources/genres/hiphop/hiphop.00000.wav'
 x , sr = librosa.load(file_path)
 
 plt.figure(figsize=(20, 6))
