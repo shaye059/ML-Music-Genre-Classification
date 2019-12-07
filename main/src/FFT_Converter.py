b3d95c489333fcb32edd19e3624fa9e7925e9845
Jonathan PC <boerger.jonathan@gmail.com>
2019-12-06 20:40:19 -0500

    Report all done (still need to run everything IOT insure it is all g)

--- main/src/FFT_Converter.py
+++
@@ -1,9 +0,0 @@
-import librosa
-import matplotlib.pyplot as plt
-import librosa.display
-
-file_path = '../resources/genres/hiphop/hiphop.00000.wav'
-x , sr = librosa.load(file_path)
-
-plt.figure(figsize=(20, 6))
-librosa.display.waveplot(x, sr=sr)
